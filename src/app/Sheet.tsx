92bf3bff517c385a2671801c3bdb0a35a9238d57
classroom <aadarshaclassroom@gmail.com>
2023-12-04 14:27:26 +0545

    Initial

---
+++ src/app/Sheet.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import SheetsFields from "./SheetsFields"
+
+export function SheetDemo() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you're done.
+          </SheetDescription>
+        </SheetHeader>
+        <SheetsFields />
+        <SheetFooter>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  )
+}
