25c11a3ab735c183bebaa9cbd4b3278a29b58ee4
Bruno Pinheiro <brunocroh@gmail.com>
2023-12-04 07:56:16 -0300

    fix: behavior of loose focus when onChange is dispatch

--- src/app/SheetsFields.tsx
+++ src/app/SheetsFields.tsx
@@ -1,31 +1,50 @@
 "use client";
-import { Input } from '@/components/ui/input'
-import { Label } from '@radix-ui/react-label'
-import React, { FormEvent, useState } from 'react'
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+import React, { FormEvent, useState } from "react";
+
+const EditRoomInfo = () => {
+  const [roomName, setRoomName] = useState("");
+  const [roomSection, setRoomSection] = useState("");
+  return (
+    <>
+      <Label htmlFor="roomName">Name:</Label>
+      <Input
+        type="text"
+        name="roomName"
+        id="roomName"
+        value={roomName}
+        onChange={(e) => setRoomName(e.target.value)}
+      />
+      <Label htmlFor="roomSection">Section:</Label>
+      <Input
+        type="text"
+        name="roomSection"
+        id="roomSection"
+        value={roomSection}
+        onChange={(e) => setRoomSection(e.target.value)}
+      />
+    </>
+  );
+};
 
 const SheetsFields = () => {
-    const [roomName, setRoomName] = useState("");
-    const [roomSection, setRoomSection] = useState("");
-    const EditRoomInfo = () => {
-        return <>
-            <Label htmlFor='roomName'>Name:</Label>
-          <Input type='text' name='roomName' id='roomName' value={roomName} onChange={(e)=>setRoomName(e.target.value)}/>
-          <Label htmlFor='roomSection'>Section:</Label>
-          <Input type='text' name='roomSection' id='roomSection' value={roomSection} onChange={(e)=>setRoomSection(e.target.value)}/>
-        </>
-    }
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault()
-    }
-    return (
-      <>
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <>
       <form onSubmit={handleSubmit}>
-          <EditRoomInfo />
-    </form>
-            <br />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nihil commodi explicabo voluptates corrupti tenetur rem! Aut corporis soluta magnam.</p>
-      </>
-  )
-}
+        <EditRoomInfo />
+      </form>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nihil
+        commodi explicabo voluptates corrupti tenetur rem! Aut corporis soluta
+        magnam.
+      </p>
+    </>
+  );
+};
 
-export default SheetsFields
+export default SheetsFields;
